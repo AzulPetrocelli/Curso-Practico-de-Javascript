608530655ccee95cfd621a3edc1ba9fa52d51dca
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-04-18 21:10:54 -0300

    Calculando la mediana de salarios

---
+++ Salarios/salarios.js
@@ -0,0 +1,82 @@
+const argentina = [];
+
+argentina.push({
+  name: "Pepe",
+  salary: 1000,
+});
+argentina.push({
+  name: "Moni",
+  salary: 500,
+});
+argentina.push({
+  name: "Maria",
+  salary: 700,
+});
+argentina.push({
+  name: "Elena",
+  salary: 750,
+});
+argentina.push({
+  name: "Azul",
+  salary: 10000,
+});
+argentina.push({
+  name: "Gaston",
+  salary: 1200,
+});
+argentina.push({
+  name: "Nahuel",
+  salary: 1700,
+});
+argentina.push({
+  name: "Brian",
+  salary: 400,
+});
+argentina.push({
+  name: "Jeronimo",
+  salary: 600,
+});
+argentina.push({
+  name: "Carmen",
+  salary: 900,
+});
+argentina.push({
+  name: "Maxi",
+  salary: 350,
+});
+argentina.push({
+  name: "Ana",
+  salary: 130,
+});
+argentina.push({
+  name: "Vero",
+  salary: 1500,
+});
+argentina.push({
+  name: "Lucas",
+  salary: 200,
+});
+argentina.push({
+  name: "Roman",
+  salary: 560,
+});
+argentina.push({
+  name: "Oscar",
+  salary: 100,
+});
+argentina.push({
+  name: "Bart",
+  salary: 550,
+});
+argentina.push({
+  name: "Lisa",
+  salary: 2000,
+});
+argentina.push({
+  name: "Homero",
+  salary: 800,
+});
+argentina.push({
+  name: "Margarita",
+  salary: 1200,
+});
