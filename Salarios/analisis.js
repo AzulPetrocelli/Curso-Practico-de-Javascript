4ee88f9e759b9b7c20b95213e9c67fbf2727bdb3
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-04-19 12:56:07 -0300

    Calculando el top 10% de salarios

--- Salarios/analisis.js
+++ Salarios/analisis.js
@@ -2,7 +2,11 @@ const salarysArg = argentina.map(function(person){
   return person.salary;
 });
 
-let media
+  const listaOrdenada = salarysArg.sort(function(a,b){
+  return a - b;
+  });
+
+let media;
 
 function mediana(list){
 
@@ -11,14 +15,20 @@ function mediana(list){
   });
 
   if(listOrganized.length % 2 === 0){
-    const index1 = listOrganized [(listOrganized.length / 2) - 1]
-    const index2 = listOrganized [(listOrganized.length / 2)]
-    media =(index1 + index2) / 2
+    const index1 = listOrganized [(listOrganized.length / 2) - 1];
+    const index2 = listOrganized [(listOrganized.length / 2)];
+    media =(index1 + index2) / 2;
   }else{
-    const index = parseInt(listOrganized.length / 2)
-    media = listOrganized[index]
+    const index = parseInt(listOrganized.length / 2);
+    media = listOrganized[index];
   }
 
-  console.log("$ " + media)
+  console.log("$ " + media);
 }
  
+
+const spliceStart = ((salarysArg.length)*90)/100;
+const spliceEnd = salarysArg.length - spliceStart;
+const salaryTop10 = salarysArg.splice(spliceStart, spliceEnd,);
+
+const medianaTop10 = mediana(salaryTop10);
